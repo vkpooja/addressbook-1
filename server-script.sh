280994493fb311e05aa5c7f3e102a0f1599530ee
pooja <pooja.kalaisekaran@gmail.com>
2022-03-23 15:40:31 +0530

    file modified

--- server-script.sh
+++ server-script.sh
@@ -1,7 +1,14 @@
 sudo yum install java-1.8.0-openjdk-devel -y
 sudo yum install git -y
 sudo yum install maven -y
-
-git clone https://github.com/vkpooja/addressbook-1.git
-cd /home/ec2-user/addressbook-1
+if [ -d "addressbook-1" ]
+then 
+ echo "repository is already present"
+ cd /home/ec2-user/addressbook-1
+ git pull origin b1
+else
+ echo "cloning the repository"
+ git clone https://github.com/vkpooja/addressbook-1.git
+ cd /home/ec2-user/addressbook-1
+fi
 mvn test
