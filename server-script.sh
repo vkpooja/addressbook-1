8bd6c39b1dbda67e104c0cb1e9f9de877dbd93f2
pooja <pooja.kalaisekaran@gmail.com>
2022-03-23 12:40:36 +0530

    file modified

---
+++ server-script.sh
@@ -0,0 +1,6 @@
+sudo yum install java-1.8.0-openjdk-devel -y
+sudo yum install git -y
+sudo yum install maven -y
+
+git clone https://github.com/vkpooja/addressbook-1.git
+cd /home/ec2-user/addressbook-1
